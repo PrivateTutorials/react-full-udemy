7af260037e1a9180158dac18a520f12362bb199a
igorgo <igorgo@backbase.com>
2020-12-28 15:23:27 +0100

    seasons app was finished

--- src/SeasonDisplay.js
+++ src/SeasonDisplay.js
@@ -1,7 +1,38 @@
+import './SeasonDisplay.css';
 import React from 'react';
 
-const SeasonDisplay = () => {
-    return <div>Season display </div>
+const seasonConfig = {
+    summer: {
+        text: 'Let hit the beach',
+        iconName: 'sun'
+    },
+    winter: {
+        text: 'Burr, it is chilly',
+        iconName: 'snowflake'
+    }
+};
+
+const getSeason = (lat, month) => {
+    if (month > 2 && month < 9) {
+        // lat > 0 - we are in Northern hemisphere
+        return lat > 0 ? 'summer' : 'winter';
+    } else {
+        return lat > 0 ? 'winter' : 'summer';
+    }
+}
+
+const SeasonDisplay = (props) => {
+    const season = getSeason(props.latitude, new Date().getMonth());
+    const {text, iconName} = seasonConfig[season];
+
+    // .season-display class matches the Component name - good practice to have outer wrapper like this
+    return (
+        <div className={`season-display ${season}`}>
+            <i className={`icon-left icon ${iconName} massive`}/>
+            <h1>{text}</h1>
+            <i className={`icon-right icon ${iconName} massive`}/>
+        </div>
+    );
 }
 
 export default SeasonDisplay;
--- src/index.js
+++ src/index.js
@@ -1,15 +1,30 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import SeasonDisplay from './SeasonDisplay';
+import Spinner from './Spenner';
 
+// another way to add semantic ui, except having it in index.html is:
+// npm install semantic-ui-css
+// import "semantic-ui-css/semantic.min.css"; in this file
+
+// CLASS component example
 class App extends React.Component {
-    constructor(props) {
-        super(props);
+    state = {
+        latitude: null,
+        errorMessage: ''
+    };
 
-        this.state = {
-            latitude: null,
-            errorMessage: ''
-        };
+    /*    constructor(props) {
+            super(props);
 
+            this.state = {
+                latitude: null,
+                errorMessage: ''
+            };
+        }*/
+
+    componentDidMount() {
+        console.log('This F() is called once the component is rendered');
         window.navigator.geolocation.getCurrentPosition(
             (position) => {
                 const {latitude} = position.coords;
@@ -23,14 +38,23 @@ class App extends React.Component {
         );
     }
 
-    // render() f() is required for any react class component
+    componentDidUpdate(prevProps, prevState, snapshot) {
+        console.log('This F() is called once the component is updated, but only after render f() again re-rendered');
+    }
+
+    renderContent() {
+        if (this.state.errorMessage && !this.state.latitude) {
+            return <p>Error: {this.state.errorMessage}</p>
+        }
+        if (!this.state.errorMessage && this.state.latitude) {
+            return <SeasonDisplay latitude={this.state.latitude}/>
+        }
+        return <Spinner message="Please accept location request"/>
+    }
+
+    // render() f() is required for any react class component. It triggers each time the component/state is updated
     render() {
-        return (
-            <div>
-                <p>Latitude: {this.state.latitude}</p>
-                <p>Error: {this.state.errorMessage}</p>
-            </div>
-        )
+        return this.renderContent();
     }
 }
 
